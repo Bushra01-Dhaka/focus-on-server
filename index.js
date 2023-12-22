8719e6a37dc854aa8090c086b51121fc2d232f70
Bushra01-Dhaka <humayraanjum87@gmail.com>
2023-12-22 15:31:45 +0600

    Created task api for deleting exact id'd user task using DELETE operation

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 //middleware
@@ -43,6 +43,27 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/tasks/:id", async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await taskCollection.deleteOne(query);
+      res.send(result);
+    })
+
+    // app.patch("/tasks/:id", async(req, res) => {
+    //   const id = req.params.id;
+    //   const filter = { _id: new ObjectId(id) };
+    //   const updateStatus = req.body;
+    //   console.log(updateStatus);
+    //   const updatedDocs = {
+    //     $set: {
+    //        status: updateStatus.status
+    //     },
+    //   };
+    //   const result = await taskCollection.updateOne(filter, updatedDocs);
+    //   res.send(result);
+
+    // })
 
 
 
